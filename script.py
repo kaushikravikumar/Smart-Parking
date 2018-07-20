819df36ec142ca808ca38055c838d8f8cc2c6d2c
Kaushik Ravikumar <kaushik.ravikumar1999@gmail.com>
2018-07-20 11:33:25 -0700

    changes

--- script.py
+++ script.py
@@ -1,36 +1,48 @@
 import RPi.GPIO as GPIO
 import time
+import threading
 
-GPIO.setmode(GPIO.BCM)
+def setup_sensor():
+        GPIO.setmode(GPIO.BCM)
 
-TRIG = 23
-ECHO = 24
+        TRIG = 23
+        ECHO = 24
 
-print "Distance Measurement In Progress"
+        print "Distance Measurement In Progress"
 
-GPIO.setup(TRIG, GPIO.OUT)
-GPIO.setup(ECHO, GPIO.IN)
+        GPIO.setup(TRIG, GPIO.OUT)
+        GPIO.setup(ECHO, GPIO.IN)
 
-GPIO.output(TRIG, False)
-print "Waiting for sensor to settle"
-time.sleep(2)
+        GPIO.output(TRIG, False)
+        print "Waiting for sensor to settle"
+        time.sleep(2)
 
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
 
-while GPIO.input(ECHO) == 0:
-    pulse_start = time.time()
+def get_location():
 
-while GPIO.input(ECHO) == 1:
-    pulse_end = time.time()
+    while GPIO.input(ECHO) == 0:
+        pulse_start = time.time()
 
-pulse_duration = pulse_end - pulse_start
+    while GPIO.input(ECHO) == 1:
+        pulse_end = time.time()
 
-distance = pulse_duration * 17150
+    pulse_duration = pulse_end - pulse_start
 
-distance = round(distance, 2)
+    distance = pulse_duration * 17150
 
-print "Distance: ", distance, "cm"
+    distance = round(distance, 2)
 
-GPIO.cleanup()        
+    print "Distance: ", distance, "cm"
+
+    GPIO.cleanup()
+
+    print(my_distance_reader.distance)
+
+
+
+if __name__ == '__main__':
+    setup_sensor()
+    threading.Timer(1.0, get_location).start()
