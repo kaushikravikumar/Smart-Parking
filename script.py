80979ce9bf8b04b9de410828f03efa26be4ee00d
Kaushik Ravikumar <kaushik.ravikumar1999@gmail.com>
2018-07-20 11:37:10 -0700

    fix

--- script.py
+++ script.py
@@ -2,24 +2,25 @@ import RPi.GPIO as GPIO
 import time
 import threading
 
+TRIG = 23
+ECHO = 24
+
 def setup_sensor():
-        GPIO.setmode(GPIO.BCM)
+    
+    GPIO.setmode(GPIO.BCM)
 
-        TRIG = 23
-        ECHO = 24
+    print "Distance Measurement In Progress"
 
-        print "Distance Measurement In Progress"
+    GPIO.setup(TRIG, GPIO.OUT)
+    GPIO.setup(ECHO, GPIO.IN)
 
-        GPIO.setup(TRIG, GPIO.OUT)
-        GPIO.setup(ECHO, GPIO.IN)
+    GPIO.output(TRIG, False)
+    print "Waiting for sensor to settle"
+    time.sleep(2)
 
-        GPIO.output(TRIG, False)
-        print "Waiting for sensor to settle"
-        time.sleep(2)
-
-        GPIO.output(TRIG, True)
-        time.sleep(0.00001)
-        GPIO.output(TRIG, False)
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
 
 def get_location():
 
